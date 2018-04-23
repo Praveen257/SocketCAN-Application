c9f6465f306c9e494a6cb6e7d4b668939214e872
Praveen <Praveen.Mohanram@he-weingarten.de>
2018-04-23 23:27:39 +0200

    Updated for CAN ID 0x701

--- inc/radar.h
+++ inc/radar.h
@@ -28,10 +28,28 @@ struct Cluster{
 	int interface;
 };
 
-
-	
+struct Cluster_GenInf{
+	int clust_id;
+	int clust_distlong;
+	int clust_distlat;
+	int clust_vrelLong;
+	int clust_vrelLat;
+	int clust_dycprop;
+	int clust_RCS;
+};
 
 #define Cluster_stat 0x600
+/* Definitions for Cluster genral information*/
+#define Cluster_Gen 0x701
+#define scal_dist 0.2
+#define scal_vrel 0.25
+#define offst_dtlong -500
+#define offst_dtlat -102.3
+#define offst_vrellong -128
+#define offst_vrellat -64
+#define offst_rcs -64
+
+
 #define d1 0.0
 #define d2 0.0
 #define c1 0.0
@@ -40,3 +58,4 @@ struct Cluster{
 
 extern int open_socket(int *socket_id);
 extern void Read_Cluster(int socket_id, struct Cluster *Cluster_ptr);
+extern void Read_ClusterGen(int socket_id, struct Cluster_GenInf *Cluster_ptr);
